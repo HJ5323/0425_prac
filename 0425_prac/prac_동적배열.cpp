c9faf220bc774f72c70d8fa493d714c098adf4c4
HJ5323 <167291731+HJ5323@users.noreply.github.com>
2024-04-25 20:29:24 +0900

    동적배열

---
+++ 0425_prac/prac_동적배열.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// 1. 몇명의 학생 성적 평균을 구할지 입력 받기
+// 2. 입력받은 학생 수 만큼 성적을 입력받기
+// 3. 학생들의 성적 평균 산출하기
+
+int main()
+{
+	int student_number;
+	float score, score_sum = 0;
+
+	cout << "학생의 수를 입력해 주세요.: " ;
+	cin >> student_number;
+
+	int* score_array = new int[student_number];
+
+
+	for (int i = 0; i < student_number; i++)
+	{
+		cout << i + 1 << " 번 학생의 성적을 입력하세요: ";
+		cin >> score;
+		cout << "\n" << endl;
+
+		score_sum += score;
+		*(score_array + i) = score;
+
+	}
+
+	delete[] score_array;
+
+	cout << "평균 : " << score_sum / student_number << endl;
+
+	return 0;
+
+}
+
